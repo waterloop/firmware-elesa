d9030c19a06a1504c349c2a75e709461e38e1f56
xander hayhoe <xanderhayhoe@gmail.com>
2023-09-23 17:05:54 -0400

    more cleanup

--- Backend/model/express_models.js
+++ Backend/model/express_models.js
@@ -1,10 +1,10 @@
 class BMS_Data {
     constructor() {
+        this.temperature = new Array();
+        this.voltage = new Array();
+        this.pack1_current = 0;
+        this.pack2_current = 0;
+        this.pack1_Voltage = 0;
+        this.pack2_Voltage = 0;
     }
-    temperature = new Array();
-    voltage = new Array();
-    pack1_current = 0;
-    pack2_current = 0;
-    pack1_Voltage = 0;
-    pack2_Voltage = 0;
 }
