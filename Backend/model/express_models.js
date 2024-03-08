85f37c3c0ccd8412d914a76b5618a1ea1a032277
Chiara Alcantara <92611941+chiaraalcantara@users.noreply.github.com>
2024-03-07 19:34:19 -0500

    86drjw1hv make gleek classes

--- Backend/model/express_models.js
+++ Backend/model/express_models.js
@@ -8,3 +8,53 @@ class BMS_Data {
         this.pack2_Voltage = 0;
     }
 }
+class BMS_DATA_MESSAGE {
+    constructor() {
+        this.id = 0;
+        this.timestamp = "";
+        this.voltage = 0.0;
+        this.current = 0.0;
+        this.battery_temperature = new Array(6);
+        this.error_message = "";
+    }   
+}
+
+class LIM_DATA_MESSAGE{
+    constructor() {
+        this.id = 0;
+        this.timestamp = "";
+        this.temperature = new Array(6);
+    }   
+}
+
+class IMU_DATA_MESSAGE{
+    constructor() {
+        this.id = 0;
+        this.timestamp = "";
+        this.imu_data =  0.0;
+    }   
+}
+
+class PRESSURE_SENSOR_MESSAGE{
+    constructor() {
+        this.id = 0;
+        this.timestamp = "";
+        this.pressure_sensor_data =  0.0;
+    }   
+}
+
+class STATUS_UPDATE_MESSAGE{
+    constructor() {
+        this.id = 0;
+        this.timestamp = "";
+        this.state = "";
+    }   
+}
+
+class CATCH_ALL_ERROR_MESSAGE{
+    constructor() {
+        this.id = 0;
+        this.timestamp = "";
+        this.errors = "";
+    }   
+}
